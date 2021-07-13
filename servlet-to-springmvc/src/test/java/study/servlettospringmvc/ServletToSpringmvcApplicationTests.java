71ac0d2ccf484a076c2f84fa78d0872b744bc513
sungjin <sungjin0757@naver.com>
2021-07-13 20:26:55 +0900

    Start

---
+++ servlet-to-springmvc/src/test/java/study/servlettospringmvc/ServletToSpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package study.servlettospringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServletToSpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
